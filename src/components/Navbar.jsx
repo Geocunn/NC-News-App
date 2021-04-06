9024e2a298d915807c6bb6a63345638748ac3fbd
George Cunningham <gcunningham97@icloud.com>
2021-04-06 17:13:57 +0100

    complete wireframe

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+import Nav from "react-bootstrap/Nav";
+import NavDropdown from "react-bootstrap/NavDropdown";
+
+class componentName extends Component {
+  render() {
+    return (
+      <Nav justify>
+        <Nav.Item className="nav-box">
+          <Nav.Link href="/" className="nav-text">
+            Home
+          </Nav.Link>
+        </Nav.Item>
+        <Nav.Item className="nav-box">
+          <Nav.Link href="/Articles" className="nav-text">
+            Latest
+          </Nav.Link>
+        </Nav.Item>
+        <Nav.Item className="nav-box">
+          <Nav.Link href="/Categories" className="nav-text">
+            Categories
+          </Nav.Link>
+        </Nav.Item>
+      </Nav>
+    );
+  }
+}
+
+export default componentName;
---
+++ src/components/Title.jsx
@@ -0,0 +1,5 @@
+const Title = () => {
+  return <h1 className="title">NC News</h1>;
+};
+
+export default Title;
